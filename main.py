8bc24ca09d11aa88895ca1447ec5d816a14f356c
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-06 18:55:25 +0900

    feat: set enum type for path parameter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,6 +9,20 @@ async def root():
     return {"message": "Hello World"}
 
 
+class LoonaSubUnit(str, Enum):
+    third = "third"
+    oec = "oec"
+    yyxy = "yyxy"
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: LoonaSubUnit):
+    match item_id:
+        case LoonaSubUnit.third:
+            return {"item_id": item_id, "subunit": "LOONA 1/3"}
+        case LoonaSubUnit.oec:
+            return {"item_id": item_id, "subunit": "LOONA Odd Eye Circle"}
+        case LoonaSubUnit.yyxy:
+            return {"item_id": item_id, "subunit": "LOONA yyxy"}
+        case _:
+            return {"item_id": item_id, "subunit": "LOONA"}
