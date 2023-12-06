432ed5d837221adf37d3d925e992f1bc826fd830
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-07 02:20:25 +0900

    feat: route board create

--- domain/board/router.py
+++ domain/board/router.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from fastapi import APIRouter, HTTPException
 from database import SessionLocal
 from models import Board
+from schemas.board import BoardCreateDto, BoardResponseDto
 
 router = APIRouter(
     prefix="/board",
@@ -23,3 +25,19 @@ def read(board_subject: str):
             raise HTTPException(status_code=404, detail="Board not found")
         return board
 
+
+@router.post("/", response_model=BoardResponseDto)
+def create(board: BoardCreateDto):
+    with SessionLocal() as db:
+        subject = board.subject
+        if db.query(Board).filter(Board.subject == subject).first():
+            raise HTTPException(status_code=400, detail="Board already exists")
+        print(board.model_dump())
+        b = Board(
+            subject=subject,
+            description=board.description,
+        )
+        print(b.create_date, b.id, b.subject, b.description)
+        db.add(Board(subject=subject, description=board.description))
+        db.commit()
+        return b
