101bd07a48054c6f29ce7288da2b1bf340d859a1
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-08 01:40:23 +0900

    refac: separate post router

--- domain/board/router.py
+++ domain/board/router.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from models import Board, Post
-from schemas.board import BoardCreateDto, BoardReadDto
-from schemas.post import PostCreateDto, PostReadDto, PostCreatedDto
+from models import Board
+from schemas.board import BoardCreateDto
+from ..post.router import router as post_router
 
 router = APIRouter(
     prefix="/board",
@@ -36,40 +36,4 @@ def create_board(board: BoardCreateDto, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400)
 
 
-@router.post("/{subject}", response_model=PostCreatedDto)
-def create_post(subject: str, post: PostCreateDto, db: Session = Depends(get_db)):
-    board = db.query(Board).filter(Board.subject == subject).first()
-    if board is None:
-        raise HTTPException(status_code=404, detail="Board not found")
-    try:
-        p = Post(
-            subject=post.subject,
-            content=post.content,
-            board_id=board.id,
-            create_date=datetime.now(),
-        )
-        db.add(p)
-        db.commit()
-        return p
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=400)
-
-
-@router.get("/{subject}/{post_id}", response_model=PostReadDto)
-def read_post(subject: str, post_id: int, db: Session = Depends(get_db)):
-    post = db.get(Post, post_id)
-    if post is None:
-        raise HTTPException(status_code=404, detail="Post not found")
-    board = db.query(Board, post.board_id).filter(Board.subject == subject).first()
-    if board is None:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return post
-
-
-@router.get("/{subject}", response_model=BoardReadDto)
-def read_board(subject: str, db: Session = Depends(get_db)):
-    board = db.query(Board).filter(Board.subject == subject).first()
-    if board is None:
-        raise HTTPException(status_code=404, detail="Board not found")
-    return board
+router.include_router(post_router)
---
+++ domain/post/router.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from models import Board, Post
+from schemas.board import BoardReadDto
+from schemas.post import PostCreateDto, PostReadDto, PostCreatedDto
+
+
+router = APIRouter(
+    prefix="/{subject}",
+)
+
+
+@router.post("/", response_model=PostCreatedDto)
+def create_post(subject: str, post: PostCreateDto, db: Session = Depends(get_db)):
+    board = db.query(Board).filter(Board.subject == subject).first()
+    if board is None:
+        raise HTTPException(status_code=404, detail="Board not found")
+    try:
+        p = Post(
+            subject=post.subject,
+            content=post.content,
+            board_id=board.id,
+            create_date=datetime.now(),
+        )
+        db.add(p)
+        db.commit()
+        return p
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400)
+
+
+@router.get("/{post_id}", response_model=PostReadDto)
+def read_post(subject: str, post_id: int, db: Session = Depends(get_db)):
+    post = db.get(Post, post_id)
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    board = db.query(Board, post.board_id).filter(Board.subject == subject).first()
+    if board is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
+
+
+@router.get("/", response_model=BoardReadDto)
+def read_board(subject: str, db: Session = Depends(get_db)):
+    board = db.query(Board).filter(Board.subject == subject).first()
+    if board is None:
+        raise HTTPException(status_code=404, detail="Board not found")
+    return board
