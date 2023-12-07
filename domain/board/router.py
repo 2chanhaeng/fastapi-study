51a1b707616eb31a4c9e609ce8b650ba5ece5733
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-07 18:29:56 +0900

    feat: create get post router

--- domain/board/router.py
+++ domain/board/router.py
@@ -37,6 +37,17 @@ def create_post(subject: str, post: PostCreateDto, db: Session = Depends(get_db)
         raise HTTPException(status_code=400)
 
 
+@router.get("/{subject}/{post_id}", response_model=PostReadDto)
+def read_post(subject: str, post_id: int, db: Session = Depends(get_db)):
+    post = db.get(Post, post_id)
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    board = db.query(Board, post.board_id).filter(Board.subject == subject).first()
+    if board is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
+
+
 @router.get("/{subject}", response_model=BoardReadDto)
 def read_board(subject: str, db: Session = Depends(get_db)):
     board = db.query(Board).filter(Board.subject == subject).first()
