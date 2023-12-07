5f6dd4beea553b72d22d7c2b4d36728840dc0717
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-07 15:49:30 +0900

    fix: add server default create_date cols

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from database import Base
 
 
@@ -9,7 +10,12 @@ class Board(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     subject = Column(String, nullable=False, unique=True)
     description = Column(Text)
-    create_date = Column(DateTime, nullable=False)
+    create_date = Column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+
+    def __repr__(self):
+        return f"<Board {self.subject} created as {self.create_date}>"
 
 
 class Post(Base):
@@ -18,6 +24,8 @@ class Post(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     subject = Column(String, nullable=False)
     content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
+    create_date = Column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
     board_id = Column(Integer, ForeignKey("board.id"))
     post = relationship(Board, backref="posts")
