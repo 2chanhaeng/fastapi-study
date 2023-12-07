f42e0227fed7f41e054eedc96a38452a86f3b9f5
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-07 18:25:14 +0900

    feat: create PostCreateDto

---
+++ schemas/post.py
@@ -0,0 +1,27 @@
+import datetime
+from pydantic import BaseModel, field_validator
+
+
+class PostCreateDto(BaseModel):
+    subject: str
+    content: str
+    board_id: int
+
+    @field_validator("subject")
+    def subject_must_not_be_empty(cls, v):
+        if v == "":
+            raise ValueError("must not be empty")
+        return v
+
+    @field_validator("content")
+    def content_must_not_be_empty(cls, v):
+        if v == "":
+            raise ValueError("must not be empty")
+        return v
+
+    @field_validator("board_id")
+    def board_id_must_be_positive(cls, v):
+        if v <= 0:
+            raise ValueError("wrong board id")
+        return v
+
