d9856bc91f77114cc6fd72bc1cd71b498a9e64a5
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-08 01:10:37 +0900

    fix: remove PostCreateDto. board_id

--- schemas/post.py
+++ schemas/post.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel, field_validator
 class PostCreateDto(BaseModel):
     subject: str
     content: str
-    board_id: int
 
     @field_validator("subject")
     def subject_must_not_be_empty(cls, v):
@@ -19,11 +18,6 @@ class PostCreateDto(BaseModel):
             raise ValueError("must not be empty")
         return v
 
-    @field_validator("board_id")
-    def board_id_must_be_positive(cls, v):
-        if v <= 0:
-            raise ValueError("wrong board id")
-        return v
 
 
 class PostReadsDto(BaseModel):
